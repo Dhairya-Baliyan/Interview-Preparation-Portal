782d8a5cfb9c818e3f97d473d3347eb1f3f9df8d
Dhairya Baliyan <dhairyabaliyan0812@gmail.com>
2026-09-11 20:10:23 +0530

    Set up initial project structure

---
+++ src/main/java/com/prepforge/IppApplication.java
@@ -0,0 +1,13 @@
+package com.prepforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prepforge/IppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prepforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
